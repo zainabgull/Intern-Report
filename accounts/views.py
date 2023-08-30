1ba9a1275aaf187d5518663ecfd62c9b6dcb4a82
Zainab <zainabnoor5492@gmail.com>
2023-08-30 12:27:02 +0500

    third Commit

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
 def login_view(request):
+    error_message = None
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -11,9 +12,9 @@ def login_view(request):
             login(request, user)
             return redirect('work_submission')  # Redirect to a success page
         else:
-            # Handle invalid login
-            pass
-    return render(request, 'login.html')
+            error_message = "Invalid username or password. Please try again."
+          
+    return render(request, 'login.html', {'error_message': error_message})
 
 def logout_view(request):
     logout(request)
--- report/views.py
+++ report/views.py
@@ -5,22 +5,25 @@ from .models import Intern
 from django.http import HttpResponseRedirect
 from .forms import WorkSubmissionForm
 
-# Create your views here.
-def home_page(request):
-    # return HttpResponse("<b>Hello Interns</b>")
-    return render(request, 'home.html') # templates auto search
-@login_required
 def work_submission(request):
     print('in work submission function')
+    if not request.user.is_authenticated:  # Check if the user is authenticated
+        return render(request, 'login_required_alert.html')  # Render the alert template
+    
     if request.method == 'POST':
         form = WorkSubmissionForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/success/')  # Redirect after successful submission
+        
     else:
         form = WorkSubmissionForm()
     interns = Intern.objects.all()
     print('interns list ', interns)
+
     return render(request, 'home.html', {'form': form, 'interns': interns})
+
 def success_form(request):
+    if not request.user.is_authenticated:  # Check if the user is authenticated
+        return render(request, 'login_required_alert.html')  # Render the alert template
     return render(request, 'success.html')
